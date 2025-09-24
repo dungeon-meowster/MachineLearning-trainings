5acd109dbbd494443392dc44551059db566017e6
Руденко Павел <116480397+dungeon-meowster@users.noreply.github.com>
2025-09-24 17:48:31 +0500

    Add files via upload

---
+++ homeworks/hw01_classification_and_attention/Постановка и решение задания №1/template_p01.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+def softmax(vector):
+    '''
+    vector: np.array of shape (n, m)
+    
+    return: np.array of shape (n, m)
+        Matrix where softmax is computed for every row independently
+    '''
+    nice_vector = vector - vector.max()
+    exp_vector = np.exp(nice_vector)
+    exp_denominator = np.sum(exp_vector, axis=1)[:, np.newaxis]
+    softmax_ = exp_vector / exp_denominator
+    return softmax_
+
+def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult):
+    '''
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    W_mult: np.array of shape (n_features_dec, n_features_enc)
+    
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    '''
+    scores = decoder_hidden_state.T @ W_mult @ encoder_hidden_states
+    weights = softmax(scores)
+    attention_vector = (weights @ encoder_hidden_states.T).T
+    return attention_vector
+
+def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
+    '''
+    decoder_hidden_state: np.array of shape (n_features_dec, 1)
+    encoder_hidden_states: np.array of shape (n_features_enc, n_states)
+    v_add: np.array of shape (n_features_int, 1)
+    W_add_enc: np.array of shape (n_features_int, n_features_enc)
+    W_add_dec: np.array of shape (n_features_int, n_features_dec)
+    
+    return: np.array of shape (n_features_enc, 1)
+        Final attention vector
+    '''
+    dec_part = W_add_dec @ decoder_hidden_state
+    enc_part = W_add_enc @ encoder_hidden_states
+    combined = enc_part + dec_part
+    tanh_combined = np.tanh(combined)
+    scores = v_add.T @ tanh_combined
+    weights = softmax(scores)
+    attention_vector = (weights @ encoder_hidden_states.T).T
+    return attention_vector
